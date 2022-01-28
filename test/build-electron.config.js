025b23ac7591f0f7d1dd32e585725adc4d95c550
Mikael Finstad <finstaden@gmail.com>
2022-01-28 21:48:28 +0700

    add test

---
+++ test/build-electron.config.js
@@ -0,0 +1,7 @@
+export default {
+  mainEntry: 'test/src/main/index.js',
+  preloadEntry: 'test/src/preload/index.js',
+  outDir: 'build',
+  mainTarget: 'electron16.0-main',
+  preloadTarget: 'electron16.0-preload',
+}
---
+++ test/src/preload/index.js
@@ -0,0 +1,5 @@
+import meow from 'meow';
+
+meow('');
+
+console.log('preload');
